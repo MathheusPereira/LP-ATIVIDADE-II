3359f98110ac23d888efe42e60fb490266870c70
MathheusPereira <143547979+MathheusPereira@users.noreply.github.com>
2023-11-07 15:45:05 -0300

    Update Atividade12.c

--- Atividade12.c
+++ Atividade12.c
@@ -7,16 +7,17 @@ int main()
 
     setlocale(LC_ALL, "portuguese");
 
-   int quantidadeNotas;
+    int i;
+    int quantidadeNotas;
     float nota = 0;
     int somaNotas = 0;
-    int media = 0;
+    float media = 0;
 
 
     printf("Digite a quantidade de notas: ");
     scanf("%d", &quantidadeNotas);
 
-    for (int i = 0; i < quantidadeNotas; i++)
+    for ( i = 0; i < quantidadeNotas; i++)
     {
         printf("Digite a nota %d: ", i + 1);
         scanf("%f", &nota);
@@ -25,7 +26,7 @@ int main()
 
     media = somaNotas / quantidadeNotas;
 
-    printf("A média das notas é: %.2i", media);
+    printf("A mÃ©dia das notas Ã©: %.2f", media);
 
     return 0;
 }
